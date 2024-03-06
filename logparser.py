9fc0ead678fe2d453eb7fb771be76e2c5fa649b2
TheMisfitF0x <77797280+TheMisfitF0x@users.noreply.github.com>
2024-03-06 17:45:31 -0500

    logparser cleanup (and fantasy)
    - Much blank space cleanup
    - Small comment pass
    - Big comment rant at the top of the file. Quite the read...
    - Commented out the silly + symbol if statement for item processing, since this is no longer being used for csv conversion (at least, not right now...)

--- logparser.py
+++ logparser.py
@@ -1,8 +1,21 @@
 import re
 import os.path
 import statMachComponents as appComps
+from enum import Enum
 
-def parseEvents(logFilePath, type="Sent"):
+# Enum for clarity when defining the type of event to be found and filtered.
+# May be discontinued in favor of a more complicated parser class
+# Eventually I want to drop this in favor of evaluating the whole log in one go and just adding each event to it's own 2D array,
+# And use a parser object to hold and manage those different 2D arrays.
+# Maybe something like a DataParser class that requires a log file to be constructed,
+# A dictionary or enum for all the patterns and has an updateData() function of the sort? 
+# IDK but sounds like a blast.
+class EventType(Enum):
+    Sent = 1
+    Hint = 2
+    Complete = 3
+
+def parseEvents(logFilePath, eventType = EventType.Sent):
     # Open the text file for reading and CSV file for writing (create if not exists)
     with open(logFilePath, 'r') as infile:
         # Event Patterns
@@ -16,46 +29,49 @@ def parseEvents(logFilePath, type="Sent"):
         # To become a 2D Array, convertable into a dataframe.
         sentEvents = []
 
+        # Iterates over every line
         for line in infile:
-            # Iterate over the sent events
+            # Identify Sent event
             match = sentPattern.search(line)
-            if match:
-                print("Match Found")
+            if match: # If event is Sent Event
+                
                 newRow = []
+
                 # Date and Time
                 date = match.group(1)
                 time = match.group(2)
-                dateTime = date + " " + time
+                dateTime = date + " " + time #Combine for ease.
                 newRow.append(dateTime)
 
                 # Sender
                 senderComp = match.group(3)
-                sender = senderComp[:-2]
+                sender = senderComp[:-2] # Get the name from the complete Sender
                 try:
-                    senderGame = appComps.gameCodes[senderComp[-2:]]
+                    senderGame = appComps.gameCodes[senderComp[-2:]] # Get the game from the complete Sender
                 except:
-                    senderGame = senderComp[-2:]
-
+                    senderGame = senderComp[-2:] # If the game doesn't exist or isn't found, use the complete sender with the last two digits removed.
                 newRow.append(sender)
                 newRow.append(senderGame)
 
-                # Receiver
+                # Receiver (same process as sender)
                 receiverComp = match.group(5)
                 receiver = receiverComp[:-2]
                 try:
                     receiverGame = appComps.gameCodes[receiverComp[-2:]]
                 except:
                     receiverGame = receiverComp[-2:]
-
                 newRow.append(receiver)
                 newRow.append(receiverGame)
 
                 # Item
                 item = match.group(4)
-                print("Sent Match")
-                if item.startswith("+"):
-                    item = "\'" + item
-                    print("Weird starting character found")
+                #if item.startswith("+"):
+                #    item = "\'" + item
+                #    print("Weird starting character found")
                 newRow.append(item)
+
+                # Add the completed event array to the larger 2D array
                 sentEvents.append(newRow)
-    return sentEvents
+    return sentEvents # Return the completed 2D array
+
+
